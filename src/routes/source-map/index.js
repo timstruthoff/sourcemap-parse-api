7f6561107779762f0600e86701f5ad95b86b6dc4
Tim Struthoff <tim.struthoff@gmail.com>
2018-08-10 15:16:43 +0200

    Remove GET endpoint for parsing

--- src/routes/source-map/index.js
+++ src/routes/source-map/index.js
@@ -5,7 +5,7 @@ const SourceMapStore = require('./../../components/sourcemap-store');
 
 const store = new SourceMapStore();
 
-// Midlleware for parsing the POST body into an object
+// Middleware for parsing the POST body into an object
 sourceMapRouter.use(express.json());
 
 sourceMapRouter.post('/', (req, res) => {
@@ -13,50 +13,26 @@ sourceMapRouter.post('/', (req, res) => {
   let sourceMapJSON = req.body.map;
 
   let id = store.add(sourceMapJSON);
+
   res.status(201).json({
     id,
   });
 });
 
-// QUESTION: URL format with code location in path (e.g. /:id/:line/:column)
-// or in GET parameter (e.g. /:id?line=1&column=2) ?
-sourceMapRouter.get('/:id', (req, res) => {
-  // TODO: Validate and sanitize input
-
-  let id = req.params.id;
-
-  // Checking if both line and column number are provided in the request
-  let parametersPresent = req.query.line !== undefined && req.query.column !== undefined;
-
-  // Parsing the parameters
-  let line = parseInt(req.query.line);
-  let column = parseInt(req.query.column);
-
-  if (parametersPresent) {
-    res.status(200).json(store.originalPositionFor(id, line, column));
-  } else {
-    res.status(400).json({ error: 'No line and column provied!' });
-  }
-});
-
 /**
  * Endpoint for parsing an array of location.
  */
-sourceMapRouter.post('/:id/multi-parser', (req, res) => {
+sourceMapRouter.post('/:id', (req, res) => {
   // TODO: Validate and sanitize input
   let id = req.params.id;
-
-  console.log(req.body);
   let locationsArray = req.body.errors;
 
   if (Array.isArray(locationsArray)) {
-
     let returnArray = [];
 
     // Looping over locations in request and getting original
     // location for each.
     locationsArray.forEach(locationElement => {
-      
       // Parsing the parameters
       let line = parseInt(locationElement.line);
       let column = parseInt(locationElement.column);
@@ -65,7 +41,6 @@ sourceMapRouter.post('/:id/multi-parser', (req, res) => {
       let parametersPresent = line !== undefined && column !== undefined;
 
       if (parametersPresent) {
-
         returnArray.push(store.originalPositionFor(id, line, column));
       } else {
         returnArray.push({ error: 'No line and column provided!' });
