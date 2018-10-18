cebb5cee0b8e4b0668b50ebec904e54def3856f4
Tim Struthoff <tim.struthoff@gmail.com>
2018-10-18 17:22:01 +0200

    Added support for dynamic ports

--- src/config/index.js
+++ src/config/index.js
@@ -1,5 +1,5 @@
 module.exports = {
-  PORT: 3000,
+  PORT: process.env.PORT || 3000,
   SOURCE_MAP_DELETE_TIMEOUT: 1000 * 60 * 60 * 24, // One day
   MAX_NUMBER_OF_SOURCE_MAPS: 5000,
   SOURCE_MAP_DELETE_INTERVAL: 1000 * 60, // One minute
